e7c29cadf6d926c8bc26f2ab0236106fc095dfdd
Jack Murphy <jack.murphy@edisonenergy.com>
2021-10-17 20:41:54 -0400

    add ch 13 problems

--- DataStructures_Practice/DSA_Wengrow/Chapter_13_Recursion2.cs
+++ DataStructures_Practice/DSA_Wengrow/Chapter_13_Recursion2.cs
@@ -7,15 +7,68 @@ namespace DSA_Wengrow
     public static class Chapter_13_Recursion2
     {
         //Do with sorting to avoid O(N^3) loop of 3
-        public static int Largest_3_Product(int[] arr)
+        public static long Largest_3_Product(int[] arr)
         {
-            int largest_prod = 0;
+            long largest_prod = 0;
 
             Array.Sort(arr);
 
-            largest_prod = arr[arr.Length - 1] * arr[arr.Length - 2] * arr[arr.Length - 3];
+            largest_prod = 
+                Convert.ToInt64(arr[arr.Length - 1]) *
+                Convert.ToInt64(arr[arr.Length - 2]) *
+                Convert.ToInt64(arr[arr.Length - 3]);
 
             return largest_prod;
+        }
+
+        //Find missing number in array in O(NlogN)
+        public static int Find_Missing(int[] arr)
+        {
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != i) { return i; }
+            }
+            throw new Exception("no missing index found");
+        }
+
+        //interesting having to figure out how to do this even slower than 1 loop...
+        public static int Find_Greatest_N2(int[] arr)
+        {
+            bool greatest;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                greatest = true;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] > arr[i])
+                    {
+                        greatest = false;
+                    }
+                }
+
+                if (greatest) { return arr[i]; }
+            }
+            throw new Exception("no greatest value found??");
+        }
+
+        public static int Find_Greatest_N(int[] arr)
+        {
+            var greatest = double.NegativeInfinity;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                greatest = Math.Max(greatest, i);
+            }
+            return (int)greatest;
+        }
+
+        public static int Find_Greatest_NlogN(int[] arr)
+        {
+            Array.Sort(arr);
+
+            return arr[arr.Length - 1];
         }
     }
 }
--- DataStructures_Practice/DSA_Wengrow_Tests/DSA_Tests_Chapter_13.cs
+++ DataStructures_Practice/DSA_Wengrow_Tests/DSA_Tests_Chapter_13.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using DSA_Wengrow;
+using System.Collections;
+using System.Linq;
+using System.Diagnostics;
 
 namespace DSA_Wengrow_Tests
 {
@@ -12,11 +15,58 @@ namespace DSA_Wengrow_Tests
         public void Test_Largest_3_Product()
         {
             int[] input = new int[] { 2, 3, 4, 5 };
-            int expected = 3 * 4 * 5;
+            long expected = 3 * 4 * 5;
 
             var result = Chapter_13_Recursion2.Largest_3_Product(input);
-
             Assert.Equal(expected, result);
+
+            input = Enumerable.Range(1, 100000).ToArray();
+            expected = 100000L * 99999L * 99998L;
+
+            result = Chapter_13_Recursion2.Largest_3_Product(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Find_Missing()
+        {
+            int[] input = new int[] { 9, 3, 2, 5, 6, 7, 1, 0, 4 };
+            int expected = 8;
+
+            int result = Chapter_13_Recursion2.Find_Missing(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Find_Greatest()
+        {
+            Random ran = new Random();
+            int[] input = Enumerable.Range(0, 50000).ToArray();
+            int[] randomArr = input.OrderBy(x => ran.Next()).ToArray();
+
+            int expected = 49999;
+            Stopwatch sw = Stopwatch.StartNew();
+            int result = Chapter_13_Recursion2.Find_Greatest_N2(randomArr);
+            sw.Stop();
+            var n2_time = sw.Elapsed;
+            Assert.Equal(expected, result);
+
+            sw = Stopwatch.StartNew();
+            result = Chapter_13_Recursion2.Find_Greatest_N(randomArr);
+            sw.Stop();
+            var n_time = sw.Elapsed;
+            Assert.Equal(expected, result);
+
+            sw = Stopwatch.StartNew();
+            result = Chapter_13_Recursion2.Find_Greatest_NlogN(randomArr);
+            sw.Stop();
+            var nlogN_time = sw.Elapsed;
+            Assert.Equal(expected, result);
+
+            //N2 time: 16 seconds
+            //NlogN time: .014 seconds
+            //N time: .0014 seconds
+            //N is ~10x faster than NlogN
         }
     }
 }
