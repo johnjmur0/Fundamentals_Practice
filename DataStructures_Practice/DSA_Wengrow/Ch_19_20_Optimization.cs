12a80f0486a21db951d1fac7f2b24b3b4d239337
Jack Murphy <johnjmur0@gmail.com>
2022-12-21 20:29:01 -0500

    ch 20 tests

--- DataStructures_Practice/DSA_Wengrow/Ch_19_20_Optimization.cs
+++ DataStructures_Practice/DSA_Wengrow/Ch_19_20_Optimization.cs
@@ -25,4 +25,68 @@ namespace DSA_Wengrow
             return arr;
         }
     }
+
+    // Do all in O(n), O(n + m) for two_sport_players
+    public static class Ch_20_Time_Opt
+    {
+
+        public static List<string> Two_Sport_Players(Dictionary<string, string>[] sport_1, Dictionary<string, string>[] sport_2)
+        {
+
+            var matches = new List<string>();
+
+            var map_dict = new Dictionary<string, int>();
+
+            foreach (var player in sport_1)
+            {
+                var full_name = player["first_name"] + " " + player["last_name"];
+
+                if (map_dict.TryGetValue(full_name, out int count))
+                {
+                    map_dict[full_name] = count + 1;
+                }
+                else
+                {
+                    map_dict[full_name] = 1;
+                }
+            }
+
+            foreach (var player in sport_2)
+            {
+                var full_name = player["first_name"] + " " + player["last_name"];
+
+                if (map_dict.TryGetValue(full_name, out int count))
+                {
+                    matches.Add(full_name);
+                }
+            }
+
+            return matches;
+        }
+
+        public static int? Find_Missing_Int(int[] arr)
+        {
+            return null;
+        }
+
+        public static int Find_Max_Sell(int[] arr)
+        {
+            return 0;
+        }
+
+        public static int Get_Highest_Product(int[] arr)
+        {
+            return 0;
+        }
+
+        public static double[] Sort_Temps(double[] arr)
+        {
+            return arr;
+        }
+
+        public static int[] Find_Longest_Sequence(int[] arr)
+        {
+            return arr;
+        }
+    }
 }
--- DataStructures_Practice/DSA_Wengrow_Tests/DSA_Tests_Optimization.cs
+++ DataStructures_Practice/DSA_Wengrow_Tests/DSA_Tests_Optimization.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using DSA_Wengrow;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace DSA_Wengrow_Tests
 {
@@ -38,6 +39,126 @@ namespace DSA_Wengrow_Tests
             var expected = input.Reverse();
 
             var result = Ch_19_Memory_Opt.Reverse_Array(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class DSA_Test_Speed_Opt
+    {
+        [Fact]
+        public void Test_Match_Players()
+        {
+            var jill_dict = new Dictionary<string, string>
+            {
+                { "first_name", "Jill"},
+                { "last_name", "Huang"}
+            };
+
+            var janko_dict = new Dictionary<string, string>
+            {
+                { "first_name", "Janko"},
+                { "last_name", "Barton"}
+            };
+            var wanda_dict = new Dictionary<string, string>
+            {
+                { "first_name", "Wanda"},
+                { "last_name", "Vakulskas"}
+            };
+
+            var tina_dict = new Dictionary<string, string>
+            {
+                { "first_name", "Tina"},
+                { "last_name", "Watkins"}
+            };
+
+            var arr_1 = new Dictionary<string, string>[] { jill_dict, wanda_dict, janko_dict };
+            var arr_2 = new Dictionary<string, string>[] { tina_dict, jill_dict, wanda_dict };
+
+            var expected_ret = new List<string>() { "Wanda Wakulskas", "Jill Huang" };
+
+            var result = Ch_20_Time_Opt.Two_Sport_Players(arr_1, arr_2);
+        }
+
+        [Fact]
+        public void Test_Find_Missing_Step()
+        {
+            var input = new int[] { 2, 3, 0, 6, 1, 5 };
+            var expected = 4;
+
+            var result = Ch_20_Time_Opt.Find_Missing_Int(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Find_Missing_Step_2()
+        {
+            var input = new int[] { 8, 2, 3, 9, 4, 7, 5, 0, 6 };
+            var expected = 1;
+
+            var result = Ch_20_Time_Opt.Find_Missing_Int(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Find_Max_Sell()
+        {
+            var input = new int[] { 10, 7, 5, 8, 11, 2, 6 };
+
+            var expected = 6;
+
+            var result = Ch_20_Time_Opt.Find_Max_Sell(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Get_Highest_Product()
+        {
+            var input = new int[] { 5, -10, -6, 9, 4 };
+
+            var expected = 60;
+
+            var result = Ch_20_Time_Opt.Get_Highest_Product(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Sort_Temps()
+        {
+            var input = new double[] { 98.6, 98.0, 97.1, 99.0, 98.9, 97.8, 98.5, 98.2, 98.0, 97.1 };
+
+            var expected = input.OrderBy(x => x).ToArray();
+
+            var result = Ch_20_Time_Opt.Sort_Temps(input);
+
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void Test_Find_Longest_Sequence()
+        {
+            var input = new int[] { 10, 5, 12, 3, 55, 30, 4, 11, 2 };
+
+            var expected = new int[] { 2, 3, 4, 5 };
+
+            var result = Ch_20_Time_Opt.Find_Longest_Sequence(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Find_Longest_Sequence_2()
+        {
+            var input = new int[] { 19, 13, 15, 12, 18, 14, 17, 11 };
+
+            var expected = new int[] { 11, 12, 13, 14, 15 };
+
+            var result = Ch_20_Time_Opt.Find_Longest_Sequence(input);
 
             Assert.Equal(expected, result);
         }
